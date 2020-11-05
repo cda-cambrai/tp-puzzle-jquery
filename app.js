434cd4c85c4577a70705b4ab5f0b54bff790b3ce
Matthieu Mota <matthieumota@gmail.com>
2020-11-05 16:54:40 +0100

    Fin bonus

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ for (var i = 0; i < positions.length; i++) {
     }
 }*/
 
+// Permet de déplacer un bloc
 function moveBloc(bloc) {
     // Récupèrer la position (top, left) du bloc cliqué
     var currentX = $(bloc).css('left'); // 0px
@@ -18,7 +19,14 @@ function moveBloc(bloc) {
     // Inverser la position des blocs
     $(bloc).css({top: emptyY, left: emptyX});
     $('.bloc-empty').css({top: currentY, left: currentX});
-}
+
+    // Intervertir les ids pour connaitre la position d'un bloc
+    // et vérifier si on a gagné la partie
+    var currentId = $(bloc).attr('id');
+    var emptyId = $('.bloc-empty').attr('id');
+    $(bloc).attr('id', emptyId);
+    $('.bloc-empty').attr('id', currentId);
+} // Fin de la fonction moveBloc
 
 // On mélange le jeu avant de commencer
 for (var i = 1; i < 100; i++) {
@@ -31,6 +39,7 @@ var shots = 0; // Représente le nombre de coups
 // Sélectionne toutes les classes .bloc
 // sauf celles qui ont la classe .bloc-empty
 $('.bloc').not('.bloc-empty').click(function () {
+    // this représente bien le bloc qui est cliqué
     // Récupèrer la position (top, left) du bloc cliqué
     var currentX = $(this).css('left'); // 0px
     var currentY = $(this).css('top'); // 100px
@@ -46,7 +55,7 @@ $('.bloc').not('.bloc-empty').click(function () {
     // On empêche le déplacement de plus de 2 bloc d'écarts
     // Et aussi le déplacement en diagonale
     if (diffX > 100 || diffY > 100 || diffX >= 100 && diffY >= 100) {
-        return;
+        // return;
     }
 
     // Inverser la position des blocs
@@ -56,12 +65,41 @@ $('.bloc').not('.bloc-empty').click(function () {
     // Incrémenteur le compteur du jeu
     shots = shots + 1;
     $('#shots strong').text(shots);
+
+    // Intervertir les ids pour connaitre la position d'un bloc
+    // et vérifier si on a gagné la partie
+    var currentId = $(this).attr('id');
+    var emptyId = $('.bloc-empty').attr('id');
+    $(this).attr('id', emptyId);
+    $('.bloc-empty').attr('id', currentId);
+
+    // Vérifier si on a gagné
+    var win = true;
+    for (var i = 1; i < 16; i++) {
+        // class="bloc-i" id="i"
+        var blocId = $('.bloc-'+i).attr('id');
+
+        // Attention, blocId renvoie une chaine
+        // "15" et 15
+        if (blocId != i) {
+            // On perd si l'id ne correspond pas au bloc
+            win = false;
+        }
+    }
+
+    if (win) {
+        $('#win').show(); // On affiche une div quand on gagne
+        clearInterval(stopwatch); // Arrêt du chronomètre
+    }
 });
 
 // Chronomètre
 var seconds = 0;
 
-setInterval(function () {
+var stopwatch = setInterval(function () {
+    // On multiplie pour éviter le calcul des floats
+    // 0.01 secondes
     seconds += 0.01 * 100;
+    // On divise ensuite pour l'affichage
     $('#stopwatch strong').text(seconds / 100);
 }, 10);
