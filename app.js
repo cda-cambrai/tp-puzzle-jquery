a9fcb2320bde0c6ee5aca15a8c2c0d5bfbdc18f9
Matthieu Mota <matthieumota@gmail.com>
2020-11-05 14:28:50 +0100

    Mélanger le jeu aléatoirement

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+/*var positions = [200, 0, 300, 100];
+
+for (var i = 0; i < positions.length; i++) {
+    for (var j = 0; j < positions.length; j++) {
+        console.log('X: '+positions[i]+' -- Y: '+positions[j]);
+    }
+}*/
+
+function moveBloc(bloc) {
+    // Récupèrer la position (top, left) du bloc cliqué
+    var currentX = $(bloc).css('left'); // 0px
+    var currentY = $(bloc).css('top'); // 100px
+
+    // Récupèrer la position du bloc "vide"
+    var emptyX = $('.bloc-empty').css('left');
+    var emptyY = $('.bloc-empty').css('top');
+
+    // Inverser la position des blocs
+    $(bloc).css({top: emptyY, left: emptyX});
+    $('.bloc-empty').css({top: currentY, left: currentX});
+}
+
+// On mélange le jeu avant de commencer
+for (var i = 1; i < 100; i++) {
+    var random = Math.floor(Math.random() * (16 - 1))
+    moveBloc('.bloc-'+random);
+}
+
 $('.bloc').click(function () {
     // Récupèrer la position (top, left) du bloc cliqué
     var currentX = $(this).css('left'); // 0px
